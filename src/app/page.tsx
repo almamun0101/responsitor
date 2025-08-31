0a5cb067e291cd30df1d1352d6d932f8002a0887
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-31 18:08:57 +0600

    dark mode

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ModeToggle} from "@/components/ui/ModeToggle"
+
+const page = () => {
+  return (
+    <div>
+     <ModeToggle/>
+    </div>
+  )
+}
+
+export default page
